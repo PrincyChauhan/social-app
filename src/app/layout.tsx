d69daf974580d1942fd43dd98b14e24e1ecda3b9
PrincyChauhan <chauhanprincee7@gmail.com>
2025-04-26 17:16:37 +0530

    Sidebar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import Navbar from "@/components/Navbar";
 import { ClerkProvider } from "@clerk/nextjs";
-
+import Sidebar from "@/components/Sidebar";
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -44,7 +44,7 @@ export default function RootLayout({
                 <div className="max-w-7xl mx-auto px-4">
                   <div className="grid grid-cols-1 lg:grid-cols-12 gap-6">
                     <div className="hidden lg:block lg:cols-span-3">
-                      sidebar
+                      <Sidebar />
                     </div>
                     <div className="lg:col-span-9"> {children}</div>
                   </div>
