c0c1acdb3645cb849d3ea3925b0bf5ab4438f404
PrincyChauhan <chauhanprincee7@gmail.com>
2025-05-07 12:41:03 +0530

    Styling change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,8 +9,8 @@ export default async function Home() {
   const posts = await getPosts();
   const dbUserId = await getDbUserId();
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
-      <div className="lg:col-span-6">
+    <div className="grid grid-cols-1 lg:grid-cols-10 gap-6 relative">
+      <div className="lg:col-span-6 z-10">
         {user ? <CreatePost /> : null}
         <div className="space-y-6">
           {posts.map((post) => (
@@ -18,8 +18,7 @@ export default async function Home() {
           ))}
         </div>
       </div>
-
-      <div className="hidden lg:block lg:col-span-4 sticky top-20">
+      <div className="hidden lg:block lg:col-span-4 sticky top-20 h-fit z-0">
         <WhoToFollow />
       </div>
     </div>
